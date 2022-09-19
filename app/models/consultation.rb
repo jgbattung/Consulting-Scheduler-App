247fa3aa17974a62c2b496f66511dc20c2a4e116
Jireh Battung <jirehgbattung@gmail.com>
2022-09-19 15:43:59 +0800

    added model relationship

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,4 +1,5 @@
 class Consultation < ApplicationRecord
     has_person_name
     belongs_to :consultation_type
+    has_rich_text :notes
 end
--- app/models/consultation_type.rb
+++ app/models/consultation_type.rb
@@ -1,4 +1,5 @@
 class ConsultationType < ApplicationRecord
   belongs_to :user
   has_rich_text :description
+  has_many :consultations
 end
